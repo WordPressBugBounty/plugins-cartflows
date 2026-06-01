d59c9d13ca2dbdad55d72305cb24bc6588cef97c
Sean Murphy <sean@iamseanmurphy.com>
2026-06-01 14:22:28 +0000

    3.1.0

--- cartflows/admin-core/assets/build/editor-app.asset.php
+++ cartflows/admin-core/assets/build/editor-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '63b45ce0792c4cd342e1');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '561194ba36de2e9a4f4d');
--- cartflows/admin-core/assets/build/settings-app.asset.php
+++ cartflows/admin-core/assets/build/settings-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e8a5458fa48cb40a0daa');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'cb5b592438ec08da9c02');
